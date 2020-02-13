a17f024e6e73badd15a0921a059204be0553d136
yankasantos <yankasantos0107@gmail.com>
2020-02-12 21:42:07 -0300

    configurando o AngularFire2 e o Firebase Real Time  no projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { environment} from '../environments/environment';
+import { AngularFireModule} from '@angular/fire';
+import { AngularFireDatabaseModule} from '@angular/fire/database';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false
+  firebase: {
+    apiKey: "AIzaSyCEyE9yadAaLFZV3BmU0swXUXNUQ0wLYno",
+    authDomain: "ynkworkhour.firebaseapp.com",
+    databaseURL: "https://ynkworkhour.firebaseio.com",
+    projectId: "ynkworkhour",
+    storageBucket: "ynkworkhour.appspot.com",
+    messagingSenderId: "69094180873",
+    appId: "1:69094180873:web:5e5f3a55e5a34d04a290d8",
+    measurementId: "G-N1M9W64N67"
+  }
+};
